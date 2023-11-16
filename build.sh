c819b0dd69e59cd18193734638056d804ef7c669
Matthew Medal <matt.medal@gmail.com>
2023-11-15 17:16:30 -0800

    Use npx

--- build.sh
+++ build.sh
@@ -8,13 +8,13 @@ rm -rf build/
 
 # compile html
 cp -r html/ build/
-pug --obj "{basedir: '$PWD'}" build/
+npx pug --obj "{basedir: '$PWD'}" build/
 find build/ -type f -name *.pug -delete
 find build/ -type f -name *.jade -delete
 rm -r build/_templates/
 
 # compile css
-lessc --clean-css css/app.less build/app.css
+npx lessc --clean-css css/app.less build/app.css
 
 # copy other files
 cp CNAME build/
