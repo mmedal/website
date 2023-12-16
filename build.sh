6dd73ef3b6f8b4b44d25fe2a65bcf6c6c4106ac9
Matthew Medal <matt.medal@gmail.com>
2023-12-15 21:29:14 -0800

    Add favicon

--- build.sh
+++ build.sh
@@ -18,6 +18,6 @@ npx lessc --clean-css css/app.less build/app.css
 
 # copy other files
 cp CNAME build/
-# cp favicon/favicon-196x196.png build/
+cp static/favicon-196x196.png build/
 cp -r fonts/social-fonts build/fonts/
 cp -r fonts/BerkeleyMono build/fonts/
