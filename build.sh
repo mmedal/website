579ff6d6a2ed3e28fcb0653d5a1fd56456787280
Matthew Medal <matt.medal@gmail.com>
2023-12-15 22:28:20 -0800

    Change out social font for svgs and add itch

--- build.sh
+++ build.sh
@@ -19,5 +19,6 @@ npx lessc --clean-css css/app.less build/app.css
 # copy other files
 cp CNAME build/
 cp static/favicon-196x196.png build/
+cp static/icons.svg build/
 cp -r fonts/social-fonts build/fonts/
 cp -r fonts/BerkeleyMono build/fonts/
