4f96bfe267736cc952495b3fb2d403573afa85e5
Matthew Medal <matt.medal@gmail.com>
2023-12-15 22:42:50 -0800

    Remove old social fonts

--- build.sh
+++ build.sh
@@ -20,5 +20,4 @@ npx lessc --clean-css css/app.less build/app.css
 cp CNAME build/
 cp static/favicon-196x196.png build/
 cp static/icons.svg build/
-cp -r fonts/social-fonts build/fonts/
 cp -r fonts/BerkeleyMono build/fonts/
